8fc8343dc92fb86a63ff823d42412474198a0670
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-24 14:08:41 +0200

    Add: Create IGuestDTO interface

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IGuestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HairSaloonAPI.Interfaces.DTOs;
+
+public interface IGuestDTO : IDTO
+{
+    public string _firstName { get; }
+    public string _lastName { get; }
+    public string _phoneNumber { get; }
+    public string _emailAddress { get; }
+}
