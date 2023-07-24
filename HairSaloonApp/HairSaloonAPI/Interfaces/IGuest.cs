91afe8369d2f5efcdbf62d55f8850095b4d3f326
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-24 14:22:35 +0200

    Doc: Add comments

--- HairSaloonApp/HairSaloonAPI/Interfaces/IGuest.cs
+++ HairSaloonApp/HairSaloonAPI/Interfaces/IGuest.cs
@@ -4,11 +4,20 @@ namespace HairSaloonAPI.Interfaces;
 
 public interface IGuest
 {
+    // Properties
+
     public int _id { get; }
     public string _firstName { get; }
     public string _lastName { get; }
     public string _phoneNumber { get; }
     public string _emailAddress { get; }
 
+
+    //Methods
+
+    /// <summary>
+    /// Edit Guest information
+    /// </summary>
+    /// <param name="updatedDTO">DTO format coming from the HTTP request</param>
     public void Edit(IGuestDTO updatedDTO);
 }
