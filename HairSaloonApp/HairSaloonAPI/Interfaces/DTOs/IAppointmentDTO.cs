a323dfd85e6fdb6fbbfe6a8cd69ca50a49418cb1
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-24 13:16:08 +0200

    Add: Create IAppointmentDTO interface

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IAppointmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HairSaloonAPI.Interfaces.DTOs;
+
+public interface IAppointmentDTO : IDTO
+{
+    public DateOnly _date { get; }
+    public TimeOnly _startTime { get; }
+    public TimeOnly _endTime { get; }
+    public string _description { get; }
+}
