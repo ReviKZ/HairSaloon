72aea69899b3269a5a567d06a3d4170c4bccaea3
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-24 14:03:10 +0200

    Remove: Deleted IPerson and IPersonDTO interfaces in accordance to app logic change

--- HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IPersonDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HairSaloonAPI.Interfaces.DTOs;
-
-public interface IPersonDTO : IDTO
-{
-    public string _firstName { get; }
-    public string _lastName { get; }
-    public string _phoneNumber { get; }
-    public string _emailAddress { get; }
-}
