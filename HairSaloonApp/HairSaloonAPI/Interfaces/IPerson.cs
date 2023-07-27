dfb21663b38fd06720cab02c448df83369131beb
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:13:17 +0200

    Update: Fix some naming convetion issues

--- HairSaloonApp/HairSaloonAPI/Interfaces/IPerson.cs
+++ HairSaloonApp/HairSaloonAPI/Interfaces/IPerson.cs
@@ -7,8 +7,8 @@ public interface IPerson
 {
     public int _id { get; }
     public IUser _user { get; }
-    public string firstName { get; }
-    public string lastName { get; }
+    public string _firstName { get; }
+    public string _lastName { get; }
     public Gender _gender { get; }
     public PersonType _type { get; }
     public string _phoneNumber { get; }
