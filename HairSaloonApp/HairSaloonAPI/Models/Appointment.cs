f1ffabed7bb9335ba4f306d18998335eba789450
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:16:27 +0200

    Add: Create Appointment model

---
+++ HairSaloonApp/HairSaloonAPI/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using HairSaloonAPI.Interfaces;
+
+namespace HairSaloonAPI.Models;
+
+public class Appointment : IAppointment
+{
+    public int _id { get; set; }
+    public DateOnly _date { get; set; }
+    public TimeOnly _startTime { get; set; }
+    public TimeOnly _endTime { get; set; }
+    public IPerson _guest { get; set; }
+    public IPerson _hairDresser { get; set; }
+    public string _description { get; set; }
+    public bool _verified { get; set; }
+}
