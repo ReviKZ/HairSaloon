8dc5a0035f2206842b33535ccffca55bb99fe1ec
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 13:02:52 +0200

    Add: Create IPersonDTO interface

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IPersonDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HairSaloonAPI.Interfaces.DTOs;
+
+public interface IPersonDTO
+{
+    public string firstName { get; }
+    public string lastName { get; }
+    public string _phoneNumber { get; }
+    public string _emailAddress { get; }
+}
