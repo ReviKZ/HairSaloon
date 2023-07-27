42e520013bdefbac7d61acceeeb5962033306d44
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:18:52 +0200

    Add: Create PersonDTO model

---
+++ HairSaloonApp/HairSaloonAPI/Models/DTOs/PersonDTO.cs
@@ -0,0 +1,11 @@
+﻿using HairSaloonAPI.Interfaces.DTOs;
+
+namespace HairSaloonAPI.Models.DTOs;
+
+public class PersonDTO : IPersonDTO
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string _phoneNumber { get; set; }
+    public string _emailAddress { get; set; }
+}
