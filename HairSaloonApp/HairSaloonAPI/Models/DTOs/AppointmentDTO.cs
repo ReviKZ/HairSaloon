1ef8deca6110915b49694ff771c5bb31beae466e
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:20:10 +0200

    Add: Create AppointmentDTO model

---
+++ HairSaloonApp/HairSaloonAPI/Models/DTOs/AppointmentDTO.cs
@@ -0,0 +1,14 @@
+﻿using HairSaloonAPI.Interfaces;
+using HairSaloonAPI.Interfaces.DTOs;
+
+namespace HairSaloonAPI.Models.DTOs;
+
+public class AppointmentDTO : IAppointmentDTO
+{
+    public DateOnly _date { get; set; }
+    public TimeOnly _startTime { get; set; }
+    public TimeOnly _endTime { get; set; }
+    public IPerson _guest { get; set; }
+    public IPerson _hairDresser { get; set; }
+    public string _description { get; set; }
+}
