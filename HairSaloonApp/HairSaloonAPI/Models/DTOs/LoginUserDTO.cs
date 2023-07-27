1ced014b0c0117e35a5ad188ea48512c67840ed5
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:21:50 +0200

    Add: Create LoginUserDTO model

---
+++ HairSaloonApp/HairSaloonAPI/Models/DTOs/LoginUserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HairSaloonAPI.Interfaces.DTOs;
+
+namespace HairSaloonAPI.Models.DTOs;
+
+public class LoginUserDTO : ILoginUserDTO
+{
+    [Required]
+    public string _userName { get; }
+    [Required, MinLength(6)]
+    public string _password { get; }
+}
