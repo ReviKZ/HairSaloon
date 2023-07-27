68bebb67d946bdcf4a13acec42eafe0bb5097b51
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 13:33:45 +0200

    Add: Create ILoginUserService & IRegisterUserService intefaces Doc: Add short descriptions for the methods

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/Services/ILoginUserService.cs
@@ -0,0 +1,20 @@
+﻿namespace HairSaloonAPI.Interfaces.Services;
+
+public interface ILoginUserService
+{
+    /// <summary>
+    /// Checks whether the username already exist in the database or not.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <returns>True if the username exist, otherwise False</returns>
+    public bool CheckIfUsernameExist(string username);
+
+    /// <summary>
+    /// Hashes the password using the given salt and compares it to the database value.
+    /// </summary>
+    /// <param name="password"></param>
+    /// <param name="passwordHash"></param>
+    /// <param name="passwordSalt"></param>
+    /// <returns>True if the values match, otherwise False</returns>
+    public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+}
