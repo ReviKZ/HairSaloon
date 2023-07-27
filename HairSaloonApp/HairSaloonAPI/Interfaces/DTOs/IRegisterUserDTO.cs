9afe0f72573efa287ca9849149276fabe794a5c2
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 13:18:54 +0200

    Add: Create ILoginUserDTO & IRegisterUserDTO interfaces

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IRegisterUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HairSaloonAPI.Interfaces.DTOs;
+
+public interface IRegisterUserDTO
+{
+    [Required]
+    public string _userName { get; }
+    [Required, MinLength(6)]
+    public string _password { get; }
+    [Required, Compare("_password")]
+    public string _confirmPassword { get; }
+}
