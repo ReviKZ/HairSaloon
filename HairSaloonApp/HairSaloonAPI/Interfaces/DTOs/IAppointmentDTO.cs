dcb7b41f0932bd4fe6d5e40857b61913f5be882b
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 12:42:54 +0200

    Update: Add missing properties

--- HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IAppointmentDTO.cs
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/IAppointmentDTO.cs
@@ -5,5 +5,7 @@ public interface IAppointmentDTO
     public DateOnly _date { get; }
     public TimeOnly _startTime { get; }
     public TimeOnly _endTime { get; }
+    public IPerson _guest { get; }
+    public IPerson _hairDresser { get; }
     public string _description { get; }
 }
