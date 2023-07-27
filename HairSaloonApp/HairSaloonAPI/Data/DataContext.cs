b3566380238d213e79619a3087c43f8ad6a62291
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 17:02:56 +0200

    Add: Create DataContext class for EF

---
+++ HairSaloonApp/HairSaloonAPI/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using HairSaloonAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSaloonAPI.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext()
+    {
+    }
+    public DataContext(DbContextOptions<DbContext> options) : base(options)
+    {
+            
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder
+            .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HairSaloonDB;Trusted_Connection=true;");
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Person> Persons => Set<Person>();
+    public DbSet<Appointment> Appointments => Set<Appointment>();
+}
