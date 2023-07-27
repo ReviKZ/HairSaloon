a029854b4e56ae9f261c50262a66e3ae5564f951
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:11:26 +0200

    Add: Create User model

---
+++ HairSaloonApp/HairSaloonAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using HairSaloonAPI.Interfaces;
+
+namespace HairSaloonAPI.Models;
+
+public class User : IUser
+{
+    public int _id { get; }
+    public string _username { get; set; } = string.Empty;
+    public byte[] _passwordHash { get; set; } = new byte[32];
+    public byte[] _passwordSalt { get; set; } = new byte[32];
+}
