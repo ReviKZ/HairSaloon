cde02a0baf66c5104bd32083bf0fda00baa4d69e
ReviKZ <kovacs.zoard.1@gmail.com>
2023-07-27 16:14:26 +0200

    Add: Create Person model

---
+++ HairSaloonApp/HairSaloonAPI/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using HairSaloonAPI.Enums;
+using HairSaloonAPI.Interfaces;
+
+namespace HairSaloonAPI.Models;
+
+public class Person : IPerson
+{
+    public int _id { get; set; }
+    public IUser _user { get; set; }
+    public string _firstName { get; set; }
+    public string _lastName { get; set; }
+    public Gender _gender { get; set; }
+    public PersonType _type { get; set; }
+    public string _phoneNumber { get; set; }
+    public string _emailAddress { get; set; }
+}
