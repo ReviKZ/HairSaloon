a56ee4ec5af0937f361f5692b3c245d8219151d8
ReviKZ <kovacs.zoard.1@gmail.com>
2023-08-01 13:17:05 +0200

    Add: Create IUserService interface Doc: Add short description for methods

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace HairSaloonAPI.Interfaces.Services;
+
+public interface IUserService
+{
+    /// <summary>
+    /// Deletes the user with the given id from the database.
+    /// </summary>
+    /// <param name="id"></param>
+    public void DeleteUser(int id);
+}
