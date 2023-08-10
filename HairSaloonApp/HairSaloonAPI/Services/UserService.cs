9a0769117b49dacef4a66fed4087e12a6752ae98
ReviKZ <kovacs.zoard.1@gmail.com>
2023-08-10 06:44:15 +0200

    Add: Create UserService class and it's methods

---
+++ HairSaloonApp/HairSaloonAPI/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using HairSaloonAPI.Data;
+using HairSaloonAPI.Interfaces.Services;
+using HairSaloonAPI.Models;
+
+namespace HairSaloonAPI.Services;
+
+public class UserService : IUserService
+{
+    private DataContext _db;
+
+    public UserService(DataContext db)
+    {
+        _db = db;
+    }
+
+    public void DeleteUser(int id)
+    {
+        User _user = _db.Users.First(u => u.Id == id);
+
+        _db.Users.Remove(_user);
+        _db.SaveChanges();
+    }
+}
