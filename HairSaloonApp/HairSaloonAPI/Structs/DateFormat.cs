f18d468b114bf62012caf5427602d566a4f0243d
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-04 10:57:01 +0200

    Add: Create DateFormat & TimeFormat structs for getting and returning relevant info

---
+++ HairSaloonApp/HairSaloonAPI/Structs/DateFormat.cs
@@ -0,0 +1,71 @@
+﻿namespace HairSaloonAPI.Structs;
+
+public struct DateFormat
+{
+    private int _year;
+    private int _month;
+    private int _day;
+    public int Year
+    {
+        get
+        {
+            return _year;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Year cannot be lower than 0");
+            }
+
+            if (value > 2999)
+            {
+                throw new ArgumentOutOfRangeException("Year cannot be higher than 2999");
+            }
+
+            _year = value;
+        }
+    }
+    public int Month
+    {
+        get
+        {
+            return _month;
+        }
+        set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentOutOfRangeException("Month cannot be lower than 1");
+            }
+
+            if (value > 12)
+            {
+                throw new ArgumentOutOfRangeException("Month cannot be higher than 12");
+            }
+
+            _month = value;
+        }
+    }
+    public int Day
+    {
+        get
+        {
+            return _day;
+        }
+        set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentOutOfRangeException("Day cannot be lower than 1");
+            }
+
+            if (value > 31)
+            {
+                throw new ArgumentOutOfRangeException("Day cannot be higher than 31");
+            }
+
+            _day = value;
+        }
+    }
+}
---
+++ HairSaloonApp/HairSaloonAPI/Structs/TimeFormat.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace HairSaloonAPI.Structs;
+
+public struct TimeFormat
+{
+    private int _hour;
+    private int _minute;
+    private int _second;
+    public int Hour 
+    {
+        get
+        {
+            return _hour;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Hour cannot be lower than 0");
+            }
+
+            if (value > 23)
+            {
+                throw new ArgumentOutOfRangeException("Hour cannot be higher than 23");
+            }
+
+            _hour = value;
+        }
+    }
+
+    public int Minute
+    {
+        get
+        {
+            return _minute;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Minute cannot be lower than 0");
+            }
+
+            if (value > 59)
+            {
+                throw new ArgumentOutOfRangeException("Minute cannot be higher than 59");
+            }
+
+            _minute = value;
+        }
+    }
+    public int Second
+    {
+        get
+        {
+            return _second;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Second cannot be lower than 0");
+            }
+
+            if (value > 59)
+            {
+                throw new ArgumentOutOfRangeException("Second cannot be higher than 59");
+            }
+
+            _second = value;
+        }
+    }
+}
