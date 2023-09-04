f1c25cfe194b4917d0a813ba88cb2b6e6014adff
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-04 07:41:00 +0200

    Add: Create IRegisterDTO and RegisterDTO for Controller data collection

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/ControllerDTOs/IRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using HairSaloonAPI.Models.DTOs;
+
+namespace HairSaloonAPI.Interfaces.DTOs.ControllerDTOs;
+
+public interface IRegisterDTO
+{
+    public RegisterUserDTO user { get; set; }
+    public PersonDTO person { get; set; }
+    public string personType { get; set; }
+    public string gender { get; set; }
+
+}
---
+++ HairSaloonApp/HairSaloonAPI/Models/DTOs/ControllerDTOs/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using HairSaloonAPI.Interfaces.DTOs;
+using HairSaloonAPI.Interfaces.DTOs.ControllerDTOs;
+
+namespace HairSaloonAPI.Models.DTOs.ControllerDTOs;
+
+public class RegisterDTO : IRegisterDTO
+{
+    public RegisterUserDTO user { get; set; }
+    public PersonDTO person { get; set; }
+    public string personType { get; set; }
+    public string gender { get; set; }
+
+}
