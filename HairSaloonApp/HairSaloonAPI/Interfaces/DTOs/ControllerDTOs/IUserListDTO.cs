80a3e8b85f012f2e1977de5d1fcd5e91b992a168
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-10 11:36:04 +0200

    Add: Create FE friendly DTO class

---
+++ HairSaloonApp/HairSaloonAPI/Interfaces/DTOs/ControllerDTOs/IUserListDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace HairSaloonAPI.Interfaces.DTOs.ControllerDTOs;
+
+public interface IUserListDTO
+{
+    public string Name { get; set; }
+    public int UserId { get; set; }
+}
---
+++ HairSaloonApp/HairSaloonAPI/Models/DTOs/ControllerDTOs/UserListDTO.cs
@@ -0,0 +1,15 @@
+﻿using HairSaloonAPI.Interfaces.DTOs.ControllerDTOs;
+
+namespace HairSaloonAPI.Models.DTOs.ControllerDTOs;
+
+public class UserListDTO : IUserListDTO
+{
+    public UserListDTO(string firstName, string lastName, int userId)
+    {
+        Name = $"{firstName} {lastName}";
+        UserId = userId;
+
+    }
+    public string Name { get; set; }
+    public int UserId { get; set; }
+}
