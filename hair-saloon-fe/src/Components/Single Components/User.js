fbcdab200046723cb0d284439c99a14f55a72509
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-11 22:48:24 +0200

    Fix: Fix existing bug with id passing

--- hair-saloon-fe/src/Components/Single Components/User.js
+++ hair-saloon-fe/src/Components/Single Components/User.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import Fetch from "../Shared Components/Fetch";
 
-const User = (id) => {
+const User = ({ id }) => {
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(false);
 
     useEffect(() => {
         const dataFetch = async () => {
-            const data = await Fetch("get", "user/" + id == 0 ? localStorage.getItem("userId") : id, "");
+            const data = await Fetch("get", "user/" + id, "");
             setUser(data);
             setLoading(false);
         }
@@ -22,8 +22,8 @@ const User = (id) => {
                 : user ? (
                     <div>
                         <h3>{user.firstName} {user.lastName}</h3>
-                        <p>Gender: {user.gender == 0 ? Male : Female}</p>
-                        <p>Position: {user.personType == 0 ? Hairdresser : Guest}</p>
+                        <p>Gender: {user.gender === 0 ? <>Male</> : <>Female</>}</p>
+                        <p>Position: {user.personType === 0 ? <>Hairdresser</> : <>Guest</>}</p>
                         <p>Email: {user.emailAddress}</p>
                         <p>Phone: {user.phoneNumber}</p>
                     </div>
