e99fe71b8beb757d7f5ea2a035ed9aaa53d26c6e
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-13 10:34:05 +0200

    Style: Add styling

--- hair-saloon-fe/src/Components/Single Components/Appointment.js
+++ hair-saloon-fe/src/Components/Single Components/Appointment.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Fetch from "../Shared Components/Fetch";
 import { useParams } from "react-router-dom";
+import "../../Styling/Appointment.css";
 
 const Appointment = () => {
     const { id } = useParams();
@@ -32,48 +33,47 @@ const Appointment = () => {
     }, []);
 
     return (
-        <>
-            <h1>Appointment Information</h1>
-            {loading ?
-                <p>Loading...</p>
-                : appointment ?
-                    (
+        <div className="appointment-info-container">
+            <h1 className="info-heading">Appointment Information</h1>
+            {loading ? (
+                <p className="loading-text">Loading...</p>
+            ) : appointment ? (
+                <div className="appointment-details">
+                    <p>Date: {appointment.date.year}.{appointment.date.month}.{appointment.date.day}</p>
+                    <p>Starting Time: {appointment.startTime.hour}:{appointment.startTime.minute}</p>
+                    <p>Approx. Ending Time: {appointment.endTime.hour}:{appointment.endTime.minute}</p>
+                    <p>Status of Appointment: {appointment.verified ? 'Verified' : 'Not Verified'}</p>
+                    <p>Description of Appointment: {appointment.description}</p>
+                    {localStorage.getItem("userId") == hairDresser.user.id ? (
+                        <p>Your Guest : {guest.firstName} {guest.lastName}</p>
+                    ) : (
+                        <p>Your Hairdresser : {hairDresser.firstName} {hairDresser.lastName}</p>
+                    )}
+                    <small>Is there a problem with the appointment? You want to get in contact, make corrections?</small>
+                    <small>
+                        You can reach your {localStorage.getItem("userId") == hairDresser.user.id ? (
+                            <small>Guest at {guest.phoneNumber} or write to {guest.emailAddress}</small>
+                        ) : (
+                            <small>Hairdresser at {hairDresser.phoneNumber} or write to {hairDresser.emailAddress}</small>
+                        )}
+                    </small>
+                    {localStorage.getItem("userId") == hairDresser.user.id ? (
+                        <></>
+                    ) : (
                         <div>
-                            <p>Date: {appointment.date.year}.{appointment.date.month}.{appointment.date.day}</p>
-                            <p>Starting Time: {appointment.startTime.hour}:{appointment.startTime.minute}</p>
-                            <p>Approx. Ending Time: {appointment.endTime.hour}:{appointment.endTime.minute}</p>
-                            <p>Status of Appointment: {appointment.verified ? <>Verified</> : <>Not Verified</>}</p>
-                            <p>Description of Appointment: {appointment.description}</p>
-                            {localStorage.getItem("userId") == hairDresser.user.id ? (<p>Your Guest : {guest.firstName} {guest.lastName}</p>) :
-                                (<p>Your Hairdresser : {hairDresser.firstName} {hairDresser.lastName}</p>)}
-                            <small>Is there a problem with the appointment? You want to get in contact, make corrections?</small>
-                            <small>
-                                You can reach your {localStorage.getItem("userId") == hairDresser.user.id ?
-                                    (<small>Guest at {guest.phoneNumber} or write to {guest.emailAddress}</small>)
-                                    :
-                                    (<small>Hairdresser at {hairDresser.phoneNumber} or write to {hairDresser.emailAddress}</small>)
-                                }
-                            </small>
-                            {localStorage.getItem("userId") == hairDresser.user.id ?
-                                <></>
-                                :
-                                (
-                                    <div>
-                                        <small>Nothing wrong? Then verify the appointment!</small>
-                                        <button type="button" onClick={verifyAppointment}>Verify</button>
-                                    </div>
-                                )
-                            }
+                            <small>Nothing wrong? Then verify the appointment!</small>
+                            <button className="verify-button" type="button" onClick={verifyAppointment}>
+                                Verify
+                            </button>
                         </div>
-                    )
-                    :
-                    (
-                        <div>
-                            <h2>There isn't an Appointment with this id in the database.</h2>
-                        </div>
-                    )
-            }
-        </>
+                    )}
+                </div>
+            ) : (
+                <div className="no-appointment">
+                    <h2>There isn't an Appointment with this id in the database.</h2>
+                </div>
+            )}
+        </div>
     );
 }
 
