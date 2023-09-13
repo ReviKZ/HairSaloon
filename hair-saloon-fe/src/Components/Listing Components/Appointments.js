1afacabf54b67317c0ff03f2b0cdcdf572a3f953
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-13 10:35:40 +0200

    Style: Add styling

--- hair-saloon-fe/src/Components/Listing Components/Appointments.js
+++ hair-saloon-fe/src/Components/Listing Components/Appointments.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Fetch from "../Shared Components/Fetch";
 import PersonConverter from "../Else/PersonConverter";
+import "../../Styling/Appointments.css";
+
 
 const Appointments = () => {
     const [loading, setLoading] = useState(true);
@@ -17,31 +19,34 @@ const Appointments = () => {
     }, []);
 
     return (
-        <>
-            <h1>Appointments</h1>
-            {loading ?
-                <p>Loading...</p>
-                : appointments ?
-                    appointments.map((appointment) => (
-                        <div key={appointment.id}>
-                            <Link to={"/appointments/" + appointment.id}>
-                                <div>{appointment.date.year}.{appointment.date.month}.{appointment.date.day}
-                                    ({appointment.startTime.hour}:{appointment.startTime.minute})
-                                    {localStorage.getItem("userId") == appointment.hairDresserId ?
-                                        (<PersonConverter id={appointment.guestId}/>) : <></>}</div>
+        <div className="appointments-container">
+            <h1 className="appointments-heading">Appointments</h1>
+            {loading ? (
+                <p className="loading-text">Loading...</p>
+            ) : appointments ? (
+                <div className="appointment-list">
+                    {appointments.map((appointment) => (
+                        <div key={appointment.id} className="appointment-item">
+                            <Link to={`/appointments/${appointment.id}`} className="appointment-link">
+                                <div className="appointment-details">
+                                    {`${appointment.date.year}.${appointment.date.month}.${appointment.date.day} 
+                    (${appointment.startTime.hour}:${appointment.startTime.minute})`}
+                                    {localStorage.getItem("userId") == appointment.hairDresserId ? (
+                                        <PersonConverter id={appointment.guestId} />
+                                    ) : (
+                                        <></>
+                                    )}
+                                </div>
                             </Link>
                         </div>
-                    )
-                    ) :
-                    (
-                        <div>
-                            <p>
-                                You don't have any appointments currently.
-                            </p>
-                        </div>
-                    )
-            }
-        </>
+                    ))}
+                </div>
+            ) : (
+                <div className="no-appointments">
+                    <p className="no-appointments-text">You don't have any appointments currently.</p>
+                </div>
+            )}
+        </div>
     );
 }
 
