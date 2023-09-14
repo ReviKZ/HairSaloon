2d8a3f2f66288c10b3ddc947e1c7e51c483ffd99
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-14 08:46:19 +0200

    Update: Adapt to new TokenConverter

--- hair-saloon-fe/src/Components/Single Components/User.js
+++ hair-saloon-fe/src/Components/Single Components/User.js
@@ -4,15 +4,18 @@ import Fetch from "../Shared Components/Fetch";
 import TokenConverter from "../Else/TokenConverter";
 import "../../Styling/User.css";
 
-const User = ({ id }) => {
+const User = () => {
     const { personId } = useParams();
     const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(false);
+    const [id, setId] = useState();
 
     useEffect(() => {
         const dataFetch = async () => {
-            const data = await Fetch("get", `user/${id ? id : personId}`, "");
+            const idData = await TokenConverter();
+            const data = await Fetch("get", `user/${personId ? personId : idData}`, "");
+            setId(idData);
             setUser(data);
             setLoading(false);
         }
@@ -40,7 +43,7 @@ const User = ({ id }) => {
                     <p className="position">Position: {user.type === 0 ? 'Guest' : 'Hairdresser'}</p>
                     <p className="email">Email: {user.emailAddress}</p>
                     <p className="phone">Phone: {user.phoneNumber}</p>
-                    {id === TokenConverter ?
+                    {user.user.id === id ?
                         (<>
                             <button className="logout-button" type="button" onClick={LogOut}>Log out!</button><br /><br />
                             <button className="logout-button" type="button" onClick={GoToAppointments}>Appointments</button>
