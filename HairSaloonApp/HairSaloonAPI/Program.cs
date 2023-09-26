e8ee20ca1c685647748f27f0b58591f7f0caee22
ReviKZ <kovacs.zoard.1@gmail.com>
2023-09-26 08:44:30 +0200

    Add: Db connection string

--- HairSaloonApp/HairSaloonAPI/Program.cs
+++ HairSaloonApp/HairSaloonAPI/Program.cs
@@ -1,6 +1,7 @@
 using HairSaloonAPI.Data;
 using HairSaloonAPI.Interfaces.Services;
 using HairSaloonAPI.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddTransient<IAppointmentService, AppointmentService>();
 builder.Services.AddTransient<IRegisterUserService, RegisterUserService>();
